64bab17faae576a7aaebc9c4aea71e3b557b5f9e
Li Cheng <timmycheng@tencent.com>
2020-06-19 17:56:46 +0800

    Let srubber remove pipeline right away.

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/SCMPipelineManager.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/SCMPipelineManager.java
@@ -437,10 +437,6 @@ public class SCMPipelineManager implements PipelineManager {
         ScmConfigKeys.OZONE_SCM_PIPELINE_ALLOCATED_TIMEOUT,
         ScmConfigKeys.OZONE_SCM_PIPELINE_ALLOCATED_TIMEOUT_DEFAULT,
         TimeUnit.MILLISECONDS);
-    Long pipelineRemoveTimeoutInMillis =
-        conf.getTimeDuration(ScmConfigKeys.OZONE_SCM_PIPELINE_DESTROY_TIMEOUT,
-            ScmConfigKeys.OZONE_SCM_PIPELINE_DESTROY_TIMEOUT_DEFAULT,
-            TimeUnit.MILLISECONDS);
 
     List<Pipeline> candidates = stateManager.getPipelines(type, factor);
 
@@ -456,13 +452,9 @@ public class SCMPipelineManager implements PipelineManager {
         closePipeline(p, false);
       }
       // scrub pipelines who stay CLOSED for too long.
-      if (p.getPipelineState() == Pipeline.PipelineState.CLOSED &&
-          (currentTime.toEpochMilli() - p.getCreationTimestamp()
-              .toEpochMilli() >= pipelineRemoveTimeoutInMillis)) {
+      if (p.getPipelineState() == Pipeline.PipelineState.CLOSED) {
         LOG.info("Scrubbing pipeline: id: " + p.getId().toString() +
-            " since it stays at CLOSED stage for " +
-            Duration.between(currentTime, p.getCreationTimestamp())
-                .toMinutes() + " mins.");
+            " since it is at CLOSED stage.");
         removePipeline(p);
       }
     }
